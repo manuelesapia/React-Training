72e9c1b30bceb40b53bfb66cdb4e25d75f3781b9
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-03 10:11:54 +0100

    more stuff

--- Ch04/04_02/start/react-app/src/App.js
+++ Ch04/04_02/start/react-app/src/App.js
@@ -4,16 +4,24 @@ import { useState, useEffect } from 'react';
 
 function App({library}) {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("Tired");
+
     useEffect(() =>{
-      console.log(`its ${emotion} right now`);
+      console.log(`its ${emotion} around here!`);
     }, [emotion]);
 
+
+    useEffect(() => {
+      console.log(`its ${secondary} around here!`);
+    })
 
   return (
     <div className="App">
         <h1>current emotion is {emotion}</h1>
          <button onClick={() => setEmotion("sad")}>Sad</button>
          <button onClick={() => setEmotion("excited")}>excited</button>
+         <h2>Current secondary emotion is {secondary}.</h2>
+         <button onClick={() => setSecondary("Grateful")}>Grateful</button>
 
     </div>
   );
