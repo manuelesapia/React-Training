da31f61fdc883a68d4106945a1be9309dc750446
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-06 10:04:21 +0100

    passing API

--- Ch04/04_02/start/react-app/src/App.js
+++ Ch04/04_02/start/react-app/src/App.js
@@ -2,6 +2,16 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 
+function GitHubUser({name, location, avatar}){
+return (
+  <div>
+    <h1>{name}</h1>
+    <p>{location}</p>
+    <img src={avatar} height={150} alt={name}/>
+  </div>
+)
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -9,9 +19,12 @@ function App() {
     ).then((response) => response.json())
     .then(setData);
   }, [])
-  if(data) return(
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-  )
+  if(data) 
+  return <GitHubUser 
+  name ={data.name} 
+  location={data.location}
+  avatar={data.avatar_url}
+  />;
   return <h1>Data</h1>;
 }
 
