b38d814775a1e6454957551cc01484eac810793c
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-07 10:49:37 +0100

    react router

--- Ch07/07_01/start/react-app/src/index.js
+++ Ch07/07_01/start/react-app/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from"react-router-dom";
+import { App, About, Contact} from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </Routes>
+  </BrowserRouter>,
+    document.getElementById("root")
+);
