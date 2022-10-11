d59daca6c35ce06814fbb2ad062d78cda5862ae3
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-11 11:38:19 +0100

    new react stuff

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -1,8 +1,27 @@
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+import appointmentList from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+        <h1 className="text-5xl">
+        <BiCalendar className="inline-block text-red-400 align-top" /> Your Appointments</h1>
+        <AddAppointment />
+        <Search />
+
+        <ul className="divide-y">
+          {appointmentList
+          .map(appointment => (
+              <AppointmentInfo key={appointment.id} 
+              appointment={appointment}
+              />
+            ))     
+          }
+        </ul>
+
     </div>
   );
 }
