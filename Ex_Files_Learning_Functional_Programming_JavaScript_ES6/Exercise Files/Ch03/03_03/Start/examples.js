072bfc018f4a0939c4d00d2c59dfcfec00bf2c11
manuele sapia <manulon12@outlook.com>
2022-10-14 07:53:56 +0100

    minior changes

--- Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_03/Start/examples.js
+++ Ex_Files_Learning_Functional_Programming_JavaScript_ES6/Exercise Files/Ch03/03_03/Start/examples.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5];
+
+
+// for (let i = 0 < numbers.length i++){
+//     doubledNumbers.push(numbers[i] * 2);
+// }
+
+const double = x => x * 2;
+
+const doubledNumbers = numbers.map(double);
+
+console.log(doubledNumbers);
+
+
