22f8d438fcf584435d067c1f4b89c2a8cc1c1dc5
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-19 15:14:12 +0100

    react hooks

--- react-full-stack-site/my-blog/src/pages/AboutPage.js
+++ react-full-stack-site/my-blog/src/pages/AboutPage.js
@@ -1,7 +1,34 @@
+import { FaStar } from "react-icons/fa";
+import React, { useState } from 'react';
+
 const AboutPage = () => {
-    return(
-        <h1>this is the About page!</h1>
-    );
+    const createArray = (length) => [
+        ...Array(length)
+      ];
+      function Star({selected = false, onSelect}){
+        return <FaStar color={selected ? "red" : "gray"} 
+        onClick={onSelect}
+        />;
+      }
+      function StarRating( {totalStars=5} ){
+        const [selectedStars, setSelectedStars]
+        = useState(0);
+        return (
+        <>
+         <h1>this is the About page!</h1>
+         <p>rete your experience</p>
+         <br/>
+          {createArray(totalStars).map((n, i) => (
+          <Star key={i} 
+          selected={selectedStars > i} 
+          onSelect={() => setSelectedStars(i + 1)}/>
+          ))}
+          <br/>
+          <p>{selectedStars} of {totalStars}</p>
+        </>
+       )
+      }
+    return <StarRating totalStars={5}/>;
 }
 
 export default AboutPage;
