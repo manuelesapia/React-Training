e9691e78afa955ab10d48c3f59ec60992f390a44
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2022-10-20 11:25:23 +0100

    some changes

--- Ex_Files_ECMAScript_6/Exercise Files/Ch02/02_03/start/script.js
+++ Ex_Files_ECMAScript_6/Exercise Files/Ch02/02_03/start/script.js
@@ -1,12 +1,46 @@
-function print(firstName) {
-  console.log(`Hello ${firstName}`);
+// function print(firstName) {
+//   console.log(`Hello ${firstName}`);
+// }
+
+
+
+// function createEmail(firstName, price ){
+//     let shopping = 5.95;
+//     console.log(`hi ${firstName}! thanks, total price ${price}`)
+// }
+
+// createEmail("David", 127);
+
+
+//const displayPeople = [];
+
+
+const printPeople = () => {
+  let peopleList = ["jACK", "SID", "Paul", "Manuel"];
+  let idArray = []; 
+  let li = "<li>";
+  let liEnd = "</li>";
+  for (let i = 0; i < peopleList.length; i++){
+    idArray.push(li + peopleList[i].toUpperCase() + liEnd)
+  }
+  document.getElementsByClassName("app-list")[0].innerHTML = idArray.join(' ');
+  console.log(peopleList);
 }
 
 
+printPeople();
 
-function createEmail(firstName, price ){
-    let shopping = 5.95;
-    console.log(`hi ${firstName}! thanks, total price ${price}`)
-}
 
-createEmail("David", 127);
+
+
+
+
+
+
+
+
+
+
+
+
+
